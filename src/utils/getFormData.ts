c4fcb097b5f8cbe1a0a3c20c213ceb62c646bad2
sidharth-timber <sidharth@timber.me>
2025-07-05 09:47:54 +0530

    fix(TIM-5): formdata setup updated

--- src/utils/getFormData.ts
+++ src/utils/getFormData.ts
@@ -1,15 +1,25 @@
-type UniversalFormData = FormData | import('form-data');
+// utils/getFormData.ts
+export type BrowserFormDataResult = {
+  formData: FormData;
+  headers: undefined;
+  env: 'browser';
+};
 
-export async function getFormData(): Promise<{
-  formData: UniversalFormData;
+export type NodeFormDataResult = {
+  formData: import('form-data');
   headers: Record<string, string>;
-}> {
+  env: 'node';
+};
+
+export type FormDataResult = BrowserFormDataResult | NodeFormDataResult;
+
+export async function getFormData(): Promise<FormDataResult> {
   if (typeof window === 'undefined') {
     const FormDataNode = (await import('form-data')).default;
     const formData = new FormDataNode();
-    return { formData, headers: formData.getHeaders() };
+    return { formData, headers: formData.getHeaders(), env: 'node' };
   } else {
     const formData = new FormData();
-    return { formData, headers: {} };
+    return { formData, headers: undefined, env: 'browser' };
   }
 }
